f27ca085f2bdae2009d88d771c85b33097ec8ccd
cczh77500 <75877467+cczh77500@users.noreply.github.com>
2020-12-12 02:04:05 -0600

    Add files via upload

---
+++ miniprogram-20/utils/xiaojueding.js
@@ -0,0 +1,333 @@
+module.exports = [
+   {
+      id: 0,
+      option: '中午吃什么',
+      awards: [
+         {
+            name: "学一鸭腿饭",
+            color: '#EE534F'
+         },
+         {
+            name: "学二口水鸡",
+            color: '#DA70D6'
+         },
+         {
+            name: "学三麻辣香锅",
+            color: '#FFA827'
+         },
+         {
+            name: "学四担担面",
+            color: '#5C6BC0'
+         },
+         {
+            name: "学五烤肉拌饭",
+            color: '#42A5F6'
+         },
+         {
+            name: "多胖了心里没点数",
+            color: '#66BB6A'
+         },
+         {
+            name: "点外卖",
+            color: '#FFC928'
+         },
+         {
+            name: "学校西餐厅碳水满足",
+            color: '#AA47BC'
+         },
+         {
+            name: "水果一枚",
+            color: '#EC407A'
+         },
+         {
+            name: "跟舍友吃一样的",
+            color: '#FF7F50'
+         }
+      ]
+   },
+   {
+      id: 1,
+      option: '送女朋友什么礼物',
+      awards: [
+         {
+            name: "施华洛世奇",
+            color: '#EE534F'
+         },
+         {
+            name: "APM monaco",
+            color: '#AA47BC'
+         },
+         {
+            name: "GUCCI",
+            color: '#FFC928'
+         },
+         {
+            name: "AJ",
+            color: '#66BB6A'
+         },
+         {
+            name: "一个巴掌",
+            color: '#42A5F6'
+         },
+         {
+            name: "一个吻",
+            color: '#5C6BC0'
+         },
+         {
+            name: "有没有女票心里没点数",
+            color: '#EC407A'
+         },
+         {
+            name: "啥也不送",
+            color: '#FF7F50'
+         }
+      ]
+   },
+   {
+      id: 2,
+      option: '和舍友玩什么卡牌游戏',
+      awards: [
+         {
+            name: "三国杀",
+            color: '#FFA827'
+         },
+         {
+            name: "大富翁",
+            color: '#AA47BC'
+         },
+         {
+            name: "斗地主",
+            color: '#42A5F6'
+         },
+         {
+            name: "扎金花",
+            color: '#00CED1'
+         },
+         {
+            name: "电力工厂",
+            color: '#66BB6A'
+         },
+         {
+            name: "游戏王",
+            color: '#FFC928'
+         }
+         
+      ]
+   },
+   {
+      id: 3,
+      option: '抽取狼人杀身份牌',
+      awards: [
+         {
+            index: 0,
+            name: "预言家",
+            color: '#EE534F'
+         },
+         {
+            index: 1,
+            name: "狼人",
+            color: '#FF7F50'
+         },
+         {
+            index: 2,
+            name: "村民",
+            color: '#FFC928'
+         },
+         {
+            index: 3,
+            name: "狼人",
+            color: '#66BB6A'
+         },
+         {
+            index: 4,
+            name: "村民",
+            color: '#42A5F6'
+         },
+         {
+            index: 5,
+            name: "狼人",
+            color: '#AA47BC'
+         },
+         {
+            index: 6,
+            name: "村民",
+            color: '#EC407A'
+         },
+         {
+            index: 7,
+            name: "狼人",
+            color: '#EE534F'
+         },
+         {
+            index: 8,
+            name: "村民",
+            color: '#FFC928'
+         },
+         {
+            index: 9,
+            name: "猎人",
+            color: '#66BB6A'
+         },
+         {
+            index: 10,
+            name: "女巫",
+            color: '#42A5F6'
+         },
+         {
+            index: 11,
+            name: "丘比特",
+            color: '#5C6BC0'
+         },
+         {
+            index: 12,
+            name: "白痴",
+            color: '#FFC928'
+         }
+      ]
+   },
+   {
+      id: 4,
+      option: '今日穿搭',
+      awards: [
+         {
+            name: "休闲风格",
+            color: '#EE534F'
+         },
+         {
+            name: "运动风格",
+            color: '#FF7F50'
+         },
+         {
+            name: "甜美风格",
+            color: '#FFC928'
+         },
+         {
+            name: "日韩风格",
+            color: '#66BB6A'
+         },
+         {
+            name: "淑女风格",
+            color: '#42A5F6'
+         },
+         {
+            name: "文艺风格",
+            color: '#5C6BC0'
+         },
+         {
+            name: "嘻哈风格",
+            color: '#AA47BC'
+         },
+         {
+            name: "朋克风格",
+            color: '#EC407A'
+         }
+      ]
+   },
+   {
+      id: 5,
+      option: '谁去关灯',
+      awards: [
+         {
+            name: "1号床",
+            color: '#EE534F'
+         },
+         {
+            name: "2号床",
+            color: '#FF7F50'
+         },
+         {
+            name: "3号床",
+            color: '#FFC928'
+         },
+         {
+            name: "1号床",
+            color: '#66BB6A'
+         },
+         {
+            name: "4号床",
+            color: '#42A5F6'
+         },
+         {
+            name: "2号床",
+            color: '#5C6BC0'
+         },
+         {
+            name: "3号床",
+            color: '#AA47BC'
+         },
+         {
+            name: "4号床",
+            color: '#EC407A'
+         },
+         {
+            name: "1号床",
+            color: '#EE534F'
+         },
+         {
+            name: "3号床",
+            color: '#FFA827'
+         },
+         {
+            name: "2号床",
+            color: '#FFC928'
+         },
+         {
+            name: "4号床",
+            color: '#66BB6A'
+         },
+         {
+            name: "3号床",
+            color: '#42A5F6'
+         },
+         {
+            name: "1号床",
+            color: '#5C6BC0'
+         },
+         {
+            name: "2号床",
+            color: '#AA47BC'
+         },
+         {
+            name: "4号床",
+            color: '#EC407A'
+         },
+         {
+            name: "一起去",
+            color: '#C71585'
+         },
+      ]
+   },
+   {
+      id: 6,
+      option: '闹铃响了要不要起',
+      awards: [
+         {
+            name: "再睡10分钟",
+            color: '#EE534F'
+         },
+         {
+            name: "再睡5分钟",
+            color: '#FF7F50'
+         },
+         {
+            name: "再睡20分钟",
+            color: '#FFC928'
+         },
+         {
+            name: "立刻起床",
+            color: '#66BB6A'
+         },
+         {
+            name: "关掉闹钟",
+            color: '#42A5F6'
+         }
+        
+
+      ]
+   },
+   
+      
+  
+      
+   
+]
